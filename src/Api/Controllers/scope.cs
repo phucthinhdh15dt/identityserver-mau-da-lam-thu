582d8427facd467a1c1a37208fd424febc6d353e
Your Name <you@example.com>
2018-09-10 08:41:22 +0700

    first commit

---
+++ src/Api/Controllers/scope.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class scope
+    {
+        
+        public String _scopes { get; set; }
+        public scope(String scopes)
+        {
+            _scopes = scopes;
+        }
+        
+    }
+}
